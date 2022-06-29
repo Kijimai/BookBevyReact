0d1edc8e5b76fce70bd6a1c5dfc67d64e11a108c
Tculls <tanner.culley@gmail.com>
2022-06-29 15:19:02 -0700

    more book models

--- Models/Review.cs
+++ Models/Review.cs
@@ -7,7 +7,14 @@ public class Review
 {
     public int ReviewId {get; set; }
 
+    [Required(ErrorMessage ="Rating Required")]
+    [Range(0,5,  ErrorMessage ="Please add a rating")]
+    [Display(Name = "Rating")]
     public double Rating {get; set; }
+
+    [Required(ErrorMessage ="Review Required")]
+    [MinLength(1,  ErrorMessage ="Please add a review")]
+    [Display(Name = "Review Comments")]
     public string ReviewComments {get; set; }
     public int BookId {get; set; }
     public int UserId {get; set; }
